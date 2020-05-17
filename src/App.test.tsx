7db7d5baee92c97e380e29dd53618aae34c26529
Simon Gamble <simon.gamble@localz.co>
2020-05-17 17:03:17 +1000

    Update app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import { render } from '@testing-library/react'
+import App from './App'
+
+const init = () => render(<App />)
+
+describe('My App component', () => {
+  test('should render app without error ', () => {
+    const { container } = init()
+    expect(container).toMatchSnapshot()
+  })
+})
